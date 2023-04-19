ca632302512f1224e75b72f823346ab43220f019
Adnan Dakhani <dakhani.an01@gmail.com>
2023-04-19 22:21:58 +0530

    port number changed

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 
-const post = 3000;
+const post = 5000;
 
 app.get("/", (req, res) => {
     res.send("Hello World!!! This is my node app");
